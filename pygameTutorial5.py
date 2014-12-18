8bcc053768aabde2d989af0dba5d32dfeb21a5d8
Jim Veneskey <biff@BigMac.local>
2014-12-17 19:28:27 -0500

    Adding new tutorial files

--- pygameTutorial5.py
+++ pygameTutorial5.py
@@ -15,24 +15,19 @@ purple = (255,0,255)
 
 
 
-setDisplay = pygame.display.set_mode((400,300))
+setDisplay = pygame.display.set_mode((300,300))
 pygame.display.set_caption('Epic Game')
 
-setDisplay.fill(cyan)
+img = pygame.image.load('evilSquare.png')
+imgx = 10
+imgy = 10
 
-singlePixel = pygame.PixelArray(setDisplay)
-singlePixel[3][3] = black
 
-pygame.draw.line(setDisplay, blue, (389, 200), (300,70),4)
-pygame.draw.circle(setDisplay, red, (50,50), 20)
-#pygame.draw.circle(setDisplay, red, (100,100), 20, 1)
 
-pygame.draw.rect(setDisplay, purple, (100,100,200,100))
-
-pygame.draw.polygon(setDisplay, green, ((50,20), (30,40), (60,100), (200,100), (3,3)))
 
 # main game loop
 while True:
+    setDisplay.blit(img, (imgx, imgy))
     for event in pygame.event.get():
         print event
         if event.type == QUIT:
